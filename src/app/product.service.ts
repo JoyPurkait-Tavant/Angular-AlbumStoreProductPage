799e9137fb3c0ea6944f20cbae93613d992bef17
Joy Purkait <joy.purkait@tavant.com>
2019-03-29 13:46:50 +0530

    Completed the module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,3 +1,4 @@
+import { Product } from './product';
 import { Album } from './album';
 import { Http, Response } from '@angular/http';
 import { Injectable } from '@angular/core';
@@ -8,10 +9,15 @@ import { Observable } from 'rxjs/Observable';
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
+  private _productsUrl = '../assets/products.json'
 
   constructor(private _http: Http) { }
 
   getAlbum(id: number): Observable<Album>{
     return this._http.get(this._albumUrl).map((response)=><Album>response.json());
+  }
+
+  getProducts(): Observable<Product[]>{
+    return this._http.get(this._productsUrl).map((response)=><Product[]>response.json());
   }
 }
